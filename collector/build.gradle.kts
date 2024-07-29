d61047b948ae135739a1337439561850e25e10b7
sangjun park <highjun10170@gmail.com>
2024-07-29 14:28:07 +0900

    Init App

---
+++ collector/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("maven-publish")
+}
+
+android {
+    namespace = "dev.iclab.collector"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing{
+    repositories{
+        maven{
+            name="GitHubPackages"
+            url = uri("https://maven.pkg.github.com/kaist-iclab/dev.iclab.collector")
+            credentials{
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "dev.iclab.collector"
+            artifactId = "collector"
+            version = "0.0.1-alpha"
+            artifact("${layout.buildDirectory}/outputs/aar/${project.name}-release.aar")
+        }
+    }
+}
---
+++ collector/src/main/java/dev/iclab/collector/Test.kt
@@ -0,0 +1,7 @@
+package dev.iclab.collector
+
+class Test {
+    fun test(){
+        println("Hello, World!")
+    }
+}
