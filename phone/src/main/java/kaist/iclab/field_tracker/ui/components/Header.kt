0f1fadaf3a3024117e579b8b2cd96ee5f679e785
sangjun park <highjun10170@gmail.com>
2025-01-16 10:15:56 +0900

    Add Phone Main Screen

--- phone/src/main/java/kaist/iclab/field_tracker/ui/components/Header.kt
+++ phone/src/main/java/kaist/iclab/field_tracker/ui/components/Header.kt
@@ -1,43 +1,36 @@
 package kaist.iclab.field_tracker.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Header() {
-    TopAppBar(
-        title = {
-            Text(text = "Settings",
-                fontWeight = FontWeight.SemiBold)
-        },
-//        actions = {
-//            IconButton(onClick = { /* Handle menu click */ }) {
-//                Icon(
-//                    imageVector = Icons.Default.Menu,  // This is the standard menu icon
-//                    contentDescription = "Menu"
-//                )
-//            }
-//        },
-        colors = TopAppBarColors(
-            containerColor = Color.White,
-            titleContentColor = Color.Black,
-            actionIconContentColor = Color.Black,
-            scrolledContainerColor = Color.White,
-            navigationIconContentColor = Color.Black
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Settings",
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
         )
-    )
+    }
 }
 
 
