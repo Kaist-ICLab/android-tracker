3f3db6bd43c7f221382647c7a61453d948131633
= <leejunmo84@gmail.com>
2025-03-07 17:33:50 +0900

    Partially implement NotificationListener

--- tracker-library/src/main/java/kaist/iclab/tracker/listener/NotificationListener.kt
+++ tracker-library/src/main/java/kaist/iclab/tracker/listener/NotificationListener.kt
@@ -1,7 +1,39 @@
-package kaist.iclab.tracker.listener//package kaist.iclab.tracker.listener
+package kaist.iclab.tracker.listener
 import android.service.notification.NotificationListenerService
+import android.service.notification.StatusBarNotification
+import kaist.iclab.tracker.listener.core.Listener
+import kaist.iclab.tracker.listener.core.NotificationEvent
 
-class NotificationListener: NotificationListenerService(){
+class NotificationListener: Listener<NotificationEvent>, NotificationListenerService() {
+    private val receivers = mutableListOf<(NotificationEvent) -> Unit>()
 
+    override fun init() {}
+
+    override fun addListener(listener: (NotificationEvent) -> Unit) {
+        assert(!receivers.contains(listener))
+        receivers.add(listener)
+    }
+
+    override fun removeListener(listener: (NotificationEvent) -> Unit) {
+        assert(receivers.contains(listener))
+        receivers.remove(listener)
+    }
+
+    override fun onNotificationPosted(sbn: StatusBarNotification?, rankingMap: RankingMap?) {
+        super.onNotificationPosted(sbn, rankingMap)
+        for(callback in receivers) {
+            callback(NotificationEvent.Removed())
+        }
+    }
+
+    override fun onNotificationRemoved(
+        sbn: StatusBarNotification?,
+        rankingMap: RankingMap?,
+    ) {
+        super.onNotificationRemoved(sbn, rankingMap)
+        for(callback in receivers) {
+            callback(NotificationEvent(sbn, rankingMap, NotificationEventType.Removed))
+        }
+    }
 }
 
