adfcb94d3d0d716801b8f0e0e3a68bb0ba7ae67b
BarkNMeow <leejunmo84@gmail.com>
2025-03-17 13:32:25 +0900

    Implement ScreenSensor

---
+++ tracker-library/src/main/java/kaist/iclab/tracker/sensor/phone/ScreenSensor.kt
@@ -0,0 +1,79 @@
+package kaist.iclab.tracker.sensor.phone
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.Build
+import kaist.iclab.tracker.listener.BroadcastListener
+import kaist.iclab.tracker.permission.PermissionManager
+import kaist.iclab.tracker.sensor.core.BaseSensor
+import kaist.iclab.tracker.sensor.core.SensorConfig
+import kaist.iclab.tracker.sensor.core.SensorEntity
+import kaist.iclab.tracker.sensor.core.SensorState
+import kaist.iclab.tracker.storage.core.StateStorage
+
+class ScreenSensor(
+    val context: Context,
+    permissionManager: PermissionManager,
+    configStorage: StateStorage<Config>,
+    val stateStorage: StateStorage<SensorState>,
+)  : BaseSensor<ScreenSensor.Config, ScreenSensor.Entity>(
+    permissionManager, configStorage, stateStorage, Config::class, Entity::class
+) {
+    /*No attribute required... can not be data class*/
+    class Config: SensorConfig
+
+    data class Entity(
+        val received: Long,
+        val timestamp: Long,
+        val type: String,
+    ) : SensorEntity
+
+    override val permissions = listOfNotNull(
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE else null
+    ).toTypedArray()
+
+    override val foregroundServiceTypes: Array<Int> = listOfNotNull(
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE else null
+    ).toTypedArray()
+
+    override val defaultConfig = Config()
+
+    private val broadcastListener = BroadcastListener(
+        context,
+        arrayOf(
+            Intent.ACTION_SCREEN_ON,
+            Intent.ACTION_SCREEN_OFF,
+            Intent.ACTION_USER_PRESENT
+        )
+    )
+
+    private val mainCallback = { intent: Intent? ->
+        listeners.forEach { listener ->
+            val timestamp = System.currentTimeMillis()
+            listener.invoke(
+                Entity(
+                    timestamp,
+                    timestamp * 1000 * 1000, // In nanoseconds
+                    intent?.action ?: "UNKNOWN"
+                )
+            )
+        }
+
+    }
+
+    // Access to Battery Status might be supported for all android systems
+    override fun init() {
+        stateStorage.set(SensorState(SensorState.FLAG.DISABLED))
+    }
+
+
+    override fun onStart() {
+        broadcastListener.addListener(mainCallback)
+    }
+
+    override fun onStop() {
+        broadcastListener.removeListener(mainCallback)
+    }
+}
