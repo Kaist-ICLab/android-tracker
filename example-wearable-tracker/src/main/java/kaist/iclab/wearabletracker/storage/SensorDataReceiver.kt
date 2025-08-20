e62c98ff7a440c999ecec3ab4943951dac774ae2
BarkNMeow <leejunmo84@gmail.com>
2025-08-20 16:36:51 +0900

    enable background data collection in example wearable tracker

--- example-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/storage/SensorDataReceiver.kt
+++ example-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/storage/SensorDataReceiver.kt
@@ -1,25 +1,77 @@
 package kaist.iclab.wearabletracker.storage
 
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.pm.ServiceInfo
+import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import kaist.iclab.tracker.sensor.controller.BackgroundController
+import kaist.iclab.tracker.sensor.controller.ControllerState
 import kaist.iclab.tracker.sensor.core.Sensor
 import kaist.iclab.tracker.sensor.core.SensorEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+import org.koin.core.qualifier.named
 
 class SensorDataReceiver(
-    private val sensors: List<Sensor<*, SensorEntity>>
+    private val context: Context,
+    private val backgroundController: BackgroundController,
 ) {
-    private val listener = sensors.associate {
-        it.name to { e: SensorEntity -> Log.d(it.name, e.toString()); Unit }
-    }
-
-    fun registerListener() {
-        for (sensor in sensors) {
-            sensor.addListener(listener[sensor.name]!!)
+    init {
+        Log.v(SensorDataReceiver::class.simpleName, "init()")
+        val serviceIntent = Intent(context, SensorDataReceiverService::class.java)
+        CoroutineScope(Dispatchers.IO).launch {
+            backgroundController.controllerStateFlow.collect {
+                Log.v(SensorDataReceiver::class.simpleName, it.toString())
+                if(it.flag == ControllerState.FLAG.RUNNING) {
+                    context.startForegroundService(serviceIntent)
+                } else {
+                    context.stopService(serviceIntent)
+                }
+            }
         }
     }
+    class SensorDataReceiverService: Service() {
+        private val sensors by inject<List<Sensor<*, *>>>(qualifier = named("sensors"))
+        private val listener = sensors.associate {
+            it.name to { e: SensorEntity -> Log.d(it.name, e.toString()); Unit }
+        }
 
-    fun unregisterListener() {
-        for (sensor in sensors) {
-            sensor.removeListener(listener[sensor.name]!!)
+        override fun onBind(p0: Intent?): IBinder? = null
+
+        override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+            val serviceNotification = BackgroundController.ControllerService.serviceNotification
+            val postNotification = NotificationCompat.Builder(
+                this,
+                serviceNotification!!.channelId
+            )
+                .setSmallIcon(serviceNotification.icon)
+                .setContentTitle(serviceNotification.title)
+                .setContentText(serviceNotification.description)
+                .setOngoing(true)
+                .build()
+
+            this.startForeground(
+                BackgroundController.ControllerService.Companion.serviceNotification!!.notificationId,
+                postNotification,
+                ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+            )
+
+            for (sensor in sensors) {
+                sensor.addListener(listener[sensor.name]!!)
+            }
+
+            return START_STICKY
+        }
+
+        override fun onDestroy() {
+            for (sensor in sensors) {
+                sensor.removeListener(listener[sensor.name]!!)
+            }
         }
     }
 }
