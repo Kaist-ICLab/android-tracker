07fe0ef4a28f228c8f27bd9794fe3e51001d5886
BarkNMeow <leejunmo84@gmail.com>
2025-09-01 17:00:01 +0900

    Add sensor collection status log

--- test-sensor/src/main/java/com/example/sensor_test_app/MainActivity.kt
+++ test-sensor/src/main/java/com/example/sensor_test_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sensor_test_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,10 +12,18 @@ import androidx.compose.ui.Modifier
 import com.example.sensor_test_app.ui.SensorScreen
 import com.example.sensor_test_app.ui.theme.AndroidtrackerTheme
 import kaist.iclab.tracker.permission.AndroidPermissionManager
+import kaist.iclab.tracker.sensor.core.Sensor
+import kaist.iclab.tracker.sensor.core.SensorEntity
 import org.koin.android.ext.android.inject
+import org.koin.core.qualifier.named
 
 class MainActivity : ComponentActivity() {
-    val permissionManager by inject<AndroidPermissionManager>()
+    private val permissionManager by inject<AndroidPermissionManager>()
+    private val sensors by inject<List<Sensor<*, *>>>(named("sensors"))
+
+    private val listener = sensors.map { sensor ->
+        { data: SensorEntity -> Log.v(sensor.name, data.toString()); Unit }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +38,20 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        for(sensorIdx in sensors.indices) {
+            sensors[sensorIdx].addListener(listener[sensorIdx])
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        for(sensorIdx in sensors.indices) {
+            sensors[sensorIdx].removeListener(listener[sensorIdx])
         }
     }
 }
