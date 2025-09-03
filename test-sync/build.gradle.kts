7c3e51b1ff2c0420840243578c878607f1c9b059
Lee Junmo <leejunmo84@gmail.com>
2025-09-03 13:11:21 +0900

    Add test app for sync module

---
+++ test-sync/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.kotlinCompose)
+
+    kotlin("plugin.serialization") version "2.2.10"
+}
+
+android {
+    namespace = "com.example.test_sync"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.test_sync"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.compose.activity)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.graphics)
+    implementation(libs.compose.ui.tooling.preview)
+    implementation(libs.compose.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(project(":tracker-library"))
+    implementation(libs.kotlinx.serialization.json)
+
+    // koin
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+}
---
+++ test-sync/src/main/java/com/example/test_sync/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.test_sync
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import com.example.test_sync.ui.theme.AndroidtrackerTheme
+import kaist.iclab.tracker.sync.BLESyncManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.decodeFromJsonElement
+
+@Serializable
+data class TestData(
+    val test: String,
+    val test2: Int,
+)
+
+class MainActivity : ComponentActivity() {
+    private val syncManager = BLESyncManager(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        syncManager.addOnReceivedListener(listOf("test")) { it: JsonElement ->
+            Log.v("test", it.toString())
+        }
+
+        syncManager.addOnReceivedListener(listOf("json")) {
+            val testData: TestData = Json.decodeFromJsonElement(it)
+            Log.v("test2", testData.toString())
+        }
+
+        enableEdgeToEdge()
+        setContent {
+            AndroidtrackerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Button(
+                            onClick = {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    syncManager.send(
+                                        "test",
+                                        "HELLO"
+                                    )
+                                }
+                            }
+                        ) {
+                            Text("Send Text")
+                        }
+
+                        Button(
+                            onClick = {
+                                CoroutineScope(Dispatchers.IO).launch {
+                                    syncManager.send(
+                                        "test2",
+                                        TestData(
+                                            test = "HELLO",
+                                            test2 = 123
+                                        )
+                                    )
+                                }
+                            }
+                        ) {
+                            Text("Send Data")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
