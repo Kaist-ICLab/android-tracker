2a71e57bb988946eb832cfa9b56cf8dd66eab003
BarkNMeow <leejunmo84@gmail.com>
2025-09-10 19:46:49 +0900

    Add description for clear text traffic

--- tracker-library/src/main/java/kaist/iclab/tracker/sync/InternetDataChannel.kt
+++ tracker-library/src/main/java/kaist/iclab/tracker/sync/InternetDataChannel.kt
@@ -14,6 +14,7 @@ import okhttp3.Response
 /**
  * A DataChannel that uses internet to transfer data.
  * Suitable for communication between server and clients, such as a mobile phone or a smartwatch.
+ * If you are using HTTP connection, you have to allow cleartextTraffic in the Android Manifest. Please refer to test-sync module.
  *
  * One distinct characteristics of InternetDataChannel is that send() cam be used in 2 ways: for transferring data and for making a request.
  * In both cases, you can retrieve the response.
