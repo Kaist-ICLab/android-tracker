7b5df92b0920e0cae162881f16ed1e8a41f5e61a
diptags <rd.pradipta@gmail.com>
2025-11-07 12:04:02 +0900

    Change variable names

--- app-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/Constants.kt
+++ app-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/Constants.kt
@@ -28,13 +28,13 @@ object Constants {
      * Notification Channel Constants
      */
     object NotificationChannel {
-        const val PHONE_COMMUNICATION_ID = "phone_communication_channel"
-        const val PHONE_COMMUNICATION_NAME = "Phone Communication"
-        const val PHONE_COMMUNICATION_DESCRIPTION = "Notifications for phone communication status"
+        const val UPLOAD_DATA_ID = "upload_data_channel"
+        const val UPLOAD_DATA_NAME = "Upload Data"
+        const val UPLOAD_DATA_DESCRIPTION = "Notifications for data upload status"
 
-        const val FLUSH_OPERATION_ID = "flush_operation_channel"
-        const val FLUSH_OPERATION_NAME = "Flush Operation"
-        const val FLUSH_OPERATION_DESCRIPTION = "Notifications for flush operation status"
+        const val FLUSH_DATA_ID = "flush_data_channel"
+        const val FLUSH_DATA_NAME = "Flush Data"
+        const val FLUSH_DATA_DESCRIPTION = "Notifications for data flush status"
 
         const val ERROR_ID = "error_channel"
         const val ERROR_NAME = "Errors"
@@ -45,10 +45,10 @@ object Constants {
      * Notification ID Constants
      */
     object NotificationId {
-        const val PHONE_COMMUNICATION_SUCCESS = 1001
-        const val PHONE_COMMUNICATION_FAILURE = 1002
-        const val FLUSH_OPERATION_SUCCESS = 1003
-        const val FLUSH_OPERATION_FAILURE = 1004
+        const val UPLOAD_DATA_SUCCESS = 1001
+        const val UPLOAD_DATA_FAILURE = 1002
+        const val FLUSH_DATA_SUCCESS = 1003
+        const val FLUSH_DATA_FAILURE = 1004
         const val ERROR = 2000 // Base ID for errors, will be incremented for multiple errors
     }
 
@@ -56,13 +56,13 @@ object Constants {
      * Notification Message Constants
      */
     object NotificationMessage {
-        object PhoneCommunication {
+        object UploadData {
             const val SUCCESS_TITLE = "Data Sent Successfully"
             const val SUCCESS_MESSAGE = "Sensor data has been sent to phone"
             const val FAILURE_TITLE = "Data Send Failed"
         }
 
-        object FlushOperation {
+        object FlushData {
             const val SUCCESS_TITLE = "Data Flushed Successfully"
             const val SUCCESS_MESSAGE = "All sensor data has been deleted"
             const val FAILURE_TITLE = "Flush Failed"
--- app-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/helpers/NotificationHelper.kt
+++ app-wearable-tracker/src/main/java/kaist/iclab/wearabletracker/helpers/NotificationHelper.kt
@@ -20,15 +20,15 @@ object NotificationHelper {
         val channelName: String,
         val description: String
     ) {
-        PHONE_COMMUNICATION(
-            Constants.NotificationChannel.PHONE_COMMUNICATION_ID,
-            Constants.NotificationChannel.PHONE_COMMUNICATION_NAME,
-            Constants.NotificationChannel.PHONE_COMMUNICATION_DESCRIPTION
+        UPLOAD_DATA(
+            Constants.NotificationChannel.UPLOAD_DATA_ID,
+            Constants.NotificationChannel.UPLOAD_DATA_NAME,
+            Constants.NotificationChannel.UPLOAD_DATA_DESCRIPTION
         ),
-        FLUSH_OPERATION(
-            Constants.NotificationChannel.FLUSH_OPERATION_ID,
-            Constants.NotificationChannel.FLUSH_OPERATION_NAME,
-            Constants.NotificationChannel.FLUSH_OPERATION_DESCRIPTION
+        FLUSH_DATA(
+            Constants.NotificationChannel.FLUSH_DATA_ID,
+            Constants.NotificationChannel.FLUSH_DATA_NAME,
+            Constants.NotificationChannel.FLUSH_DATA_DESCRIPTION
         ),
         ERROR(
             Constants.NotificationChannel.ERROR_ID,
@@ -142,10 +142,10 @@ object NotificationHelper {
     fun showPhoneCommunicationSuccess(context: Context) {
         showSuccessNotification(
             context = context,
-            channelConfig = NotificationChannelConfig.PHONE_COMMUNICATION,
-            title = Constants.NotificationMessage.PhoneCommunication.SUCCESS_TITLE,
-            message = Constants.NotificationMessage.PhoneCommunication.SUCCESS_MESSAGE,
-            notificationId = Constants.NotificationId.PHONE_COMMUNICATION_SUCCESS
+            channelConfig = NotificationChannelConfig.UPLOAD_DATA,
+            title = Constants.NotificationMessage.UploadData.SUCCESS_TITLE,
+            message = Constants.NotificationMessage.UploadData.SUCCESS_MESSAGE,
+            notificationId = Constants.NotificationId.UPLOAD_DATA_SUCCESS
         )
     }
 
@@ -157,9 +157,9 @@ object NotificationHelper {
             context = context,
             exception = exception,
             contextInfo = contextInfo,
-            channelConfig = NotificationChannelConfig.PHONE_COMMUNICATION,
-            title = Constants.NotificationMessage.PhoneCommunication.FAILURE_TITLE,
-            notificationId = Constants.NotificationId.PHONE_COMMUNICATION_FAILURE
+            channelConfig = NotificationChannelConfig.UPLOAD_DATA,
+            title = Constants.NotificationMessage.UploadData.FAILURE_TITLE,
+            notificationId = Constants.NotificationId.UPLOAD_DATA_FAILURE
         )
     }
 
@@ -181,10 +181,10 @@ object NotificationHelper {
     fun showFlushSuccess(context: Context) {
         showSuccessNotification(
             context = context,
-            channelConfig = NotificationChannelConfig.FLUSH_OPERATION,
-            title = Constants.NotificationMessage.FlushOperation.SUCCESS_TITLE,
-            message = Constants.NotificationMessage.FlushOperation.SUCCESS_MESSAGE,
-            notificationId = Constants.NotificationId.FLUSH_OPERATION_SUCCESS
+            channelConfig = NotificationChannelConfig.FLUSH_DATA,
+            title = Constants.NotificationMessage.FlushData.SUCCESS_TITLE,
+            message = Constants.NotificationMessage.FlushData.SUCCESS_MESSAGE,
+            notificationId = Constants.NotificationId.FLUSH_DATA_SUCCESS
         )
     }
 
@@ -196,9 +196,9 @@ object NotificationHelper {
             context = context,
             exception = exception,
             contextInfo = contextInfo,
-            channelConfig = NotificationChannelConfig.FLUSH_OPERATION,
-            title = Constants.NotificationMessage.FlushOperation.FAILURE_TITLE,
-            notificationId = Constants.NotificationId.FLUSH_OPERATION_FAILURE
+            channelConfig = NotificationChannelConfig.FLUSH_DATA,
+            title = Constants.NotificationMessage.FlushData.FAILURE_TITLE,
+            notificationId = Constants.NotificationId.FLUSH_DATA_FAILURE
         )
     }
 
